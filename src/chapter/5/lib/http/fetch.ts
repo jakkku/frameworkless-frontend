6e6ed90c14bc184bf2a91b1df8c30ab658412c74
a01081440011 <a01081440011@gmail.com>
2022-07-17 21:13:44 +0900

    chapter05: impl custom fetch

---
+++ src/chapter/5/lib/http/fetch.ts
@@ -0,0 +1,65 @@
+type Params = {
+  url: string;
+  body?: any;
+  headers?: Headers;
+  method?: Method;
+};
+type Headers = { [key: string]: string };
+type Method = "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
+
+const parseResponse = async <T>(response: Response) => {
+  const { status } = response;
+  let data: T | undefined;
+
+  if (status !== 204) {
+    data = await response.json();
+  }
+
+  return { status, data };
+};
+
+const request = async <T>(params: Params) => {
+  const { method = "GET", url, headers = {}, body } = params;
+  const config: RequestInit = { method, headers: new window.Headers(headers) };
+
+  if (body) {
+    config.body = JSON.stringify(body);
+  }
+
+  const response = await fetch(url, config);
+
+  return parseResponse<T>(response);
+};
+
+const get = async <T>(url: string, body?: any, headers?: Headers) => {
+  const response = await request<T>({ url, headers, body });
+  return response.data;
+};
+
+const post = async <T>(url: string, body?: any, headers?: Headers) => {
+  const response = await request<T>({ url, headers, method: "POST", body });
+  return response.data;
+};
+
+const put = async <T>(url: string, body?: any, headers?: Headers) => {
+  const response = await request<T>({ url, headers, method: "PUT", body });
+  return response.data;
+};
+
+const patch = async <T>(url: string, body?: any, headers?: Headers) => {
+  const response = await request<T>({ url, headers, method: "PATCH", body });
+  return response.data;
+};
+
+const deleteRequest = async <T>(url: string, body?: any, headers?: Headers) => {
+  const response = await request<T>({ url, headers, method: "DELETE" });
+  return response.data;
+};
+
+export default {
+  get,
+  post,
+  put,
+  patch,
+  delete: deleteRequest,
+};
